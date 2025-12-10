e56e503beb03e1b5c3c890bab00e8294baf6965c
Rychard Fundacao <rychard.professional@gmail.com>
2025-12-10 07:46:10 -0400

    refac: prepare to new database

--- jest.setup.js
+++ jest.setup.js
@@ -1,2 +1,8 @@
 // Learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom"
+import { TextEncoder, TextDecoder } from "util"
+
+global.TextEncoder = TextEncoder
+global.TextDecoder = TextDecoder
+
+console.log("NEXT_PUBLIC_VERCEL_URL:", process.env.NEXT_PUBLIC_VERCEL_URL)
